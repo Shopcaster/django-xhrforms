3c8ddb8d636f5925008b393d703fa96ba5a8811c
Aron Jones <defrex0@gmail.com>
2012-11-14 11:29:21 -0500

    licence, setup.py

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 django-xhrforms
 ================
 
-Simple XHR form validation in Django
+Simple XHR form validation in Django.
+
+Dependancies
+------------
+
+* jQyery >= 1.7
+* Bootstrap (optional: all HTML is bootstrap-style)
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-xhrforms',
+    version='0.1.0',
+    author='Aron Jones',
+    author_email='aron.jones@gmail.com',
+    packages=['xhrforms'],
+    url='https://github.com/Shopcaster/django-xhr-form',
+    license='LICENSE.txt',
+    description='Simple XHR form validation in Django.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.4.0",
+    ],
+)
